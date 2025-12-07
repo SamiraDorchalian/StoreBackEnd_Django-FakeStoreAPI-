4a5194943f226f7791b1632b048cf1460d41897a
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-12-07 17:16:25 +0330

    fix: add cors headers

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'rest_framework',  # for API
     'drf_yasg',  # for Swagger
     'products',  #app
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -53,6 +56,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://storefrontend-react.onrender.com",
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
